72053297cb2cbbf9f3dc4c6528fb7c1e92dd1504
sjoe77 <softwarejoe77@gmail.com>
2026-04-01 17:17:25 -0400

    Add clean layout: hide sidebar, header, breadcrumbs

---
+++ evidence/pages/+layout.svelte
@@ -0,0 +1,16 @@
+<EvidenceDefaultLayout {data}
+  hideSidebar={true}
+  hideHeader={true}
+  hideBreadcrumbs={true}
+  hideTOC={true}
+  fullWidth={false}
+  builtWithEvidence={false}
+  neverShowQueries={true}
+>
+  <slot slot="content" />
+</EvidenceDefaultLayout>
+
+<script>
+  import { EvidenceDefaultLayout } from "@evidence-dev/core-components";
+  export let data;
+</script>
